d1bd451bff53842d618d8307aa0bbdd46a9edd19
phuruipat6509611965 <phuruipat.kon@dome.tu.ac.th>
2023-11-17 02:49:33 +0700

    Add data and make json string

--- src/main/java/com/example/cs264/controller/teacherPageController.java
+++ src/main/java/com/example/cs264/controller/teacherPageController.java
@@ -31,8 +31,13 @@ public class teacherPageController {
     }
 
     //Use to approve or deny student's subject(subjectTeacherApprove true or false) and change subjectTeacherCheck to true
-    @PostMapping("/tcheck")
-    public void teacherApprove(@RequestBody Student  student, @RequestParam(name = "check", required = true) boolean check, @RequestParam(name = "subjectCode", required = true) String subjectCode){
-        teacherRepository.teacherApprove(student, check, subjectCode);
+    @GetMapping("/tcheck")
+    public boolean teacherApprove(@RequestParam(name = "check", required = true) boolean check, @RequestParam(name = "subjectCode", required = true) String subjectCode, @RequestBody Student  student){
+        try {
+            teacherRepository.teacherApprove(student, check, subjectCode);
+            return  true;
+        }catch (Exception e){
+            return false;
+        }
     }
 }
